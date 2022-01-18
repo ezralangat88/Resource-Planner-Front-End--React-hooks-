795fbe58e2e6401b7486e2823593c271cdeac2e9
Ezra <ezra.dev98@gmail.com>
2022-01-18 15:41:39 +0300

    added Header and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import FooterComponent from './Components/FooterComponent';
+import HeaderComponent from './Components/HeaderComponent';
 import ListUsersComponent from './Components/ListUsersComponent';
 
 function App() {
   return (
     <div>
+      <HeaderComponent/>
       <ListUsersComponent/>
+      <FooterComponent/>
     </div>
   );
 }
